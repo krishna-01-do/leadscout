420febbd9505989d78ce71ecf87d57584fb4c008
CodexSandboxOffline <sahanikrishna0000@gmail.com>
2026-09-21 22:32:00 +0530

    Add Meta Pixel tracking sitewide

--- app/layout.tsx
+++ app/layout.tsx
@@ -45,6 +45,24 @@ function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 gtag('config', 'G-D5V2N8QQ0Q');`}
         </Script>
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '2106522826925737');
+fbq('track', 'PageView');`}
+        </Script>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html:
+              '<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=2106522826925737&ev=PageView&noscript=1" alt="" />',
+          }}
+        />
       </head>
       <body className={inter.className} suppressHydrationWarning><AuthProvider>{children}</AuthProvider></body>
     </html>
